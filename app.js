dd3f035671cc944eea2fec5d4c332d4db1c7ce82
DaseinL <daseinwelt@outlook.com>
2021-11-21 05:50:19 +0000

    Changes

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ app.use((req,res,next)=>{
     res.status(404).send('404 No found');
 });
 
-app.listen(88);
+app.listen(8080);
 
-console.log('This page runs on http://127.0.0.1:88');
+console.log('This page runs on http://127.0.0.1:8080');
