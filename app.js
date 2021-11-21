eaab47b977557a55c6e16bb27754bbd717d2ed95
DaseinL <daseinwelt@outlook.com>
2021-11-21 05:44:23 +0000

    Changes

---
+++ app.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+app.use(express.static('resource'));
+app.use(express.json());
+
+app.get('/',function(req,res){
+    res.status(200).send('index.html');
+});
+
+app.use((req,res,next)=>{
+    res.status(404).send('404 No found');
+});
+
+app.listen(88);
+
+console.log('This page runs on http://127.0.0.1:88');
